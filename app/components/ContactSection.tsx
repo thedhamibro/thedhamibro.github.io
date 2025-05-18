5e26a39ed999f44391a24eab640c01f81e3b79f7
Sehajdeep Singh Dhami <dhamiputtar2009@gmail.com>
2025-05-18 19:27:31 +0530

    Update ContactSection.tsx

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -45,7 +45,7 @@ export default function ContactSection() {
 							</motion.a>
 							<p className="text-green-500 mt-8 mb-2">$ cat resume.pdf</p>
 							<motion.a
-								href="/path-to-resume.pdf"
+								href="https://GitHub.com/thedhamibro/thedhamibro.github.io"
 								target="_blank"
 								rel="noopener noreferrer"
 								whileHover={{ scale: 1.02 }}
